f9c3f46f3b65ff161380c5f5e53254e8f580664d
Christine Arroyo <chrstinearroyo@gmail.com>
2025-03-14 08:44:43 +0800

    Update Arroyo_PS.py

--- Arroyo_PS.py
+++ Arroyo_PS.py
@@ -1,11 +1,18 @@
 #! C:\Users\chrst\OneDrive\Documents\GitHub\NumPySymPy-Assignment\myenv\Scripts\python.exe
-
+'''
 import numpy as np
 
 def is_subset(set_a, set_b):
-    return np.all(np.isin(set_a, set_b))
+    
+    array_a = np.array(list(set_a))
+    array_b = np.array(list(set_b))
+
+    result = np.isin(array_a, array_b).all()
+    return result
     
 set_a = {1,3,5}
 set_b = {2, 1, 4, 3, 6, 5}
 
 print("Is set_a a subset of set_b? ", is_subset(set_a, set_b))
+'''
+
