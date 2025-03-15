2f04caaa9d786f93776de0723a2d83b4b3e8bfc9
Christine Arroyo <chrstinearroyo@gmail.com>
2025-03-15 14:46:28 +0800

    Update Arroyo_PS.py

--- Arroyo_PS.py
+++ Arroyo_PS.py
@@ -1,4 +1,33 @@
-from sympy import FiniteSet
+
+# Function to generate power set
+def power_set(s):
+    if not s:
+        return [[]]
+    first = s[0]
+    rest = power_set(s[1:])
+    return rest + [[first] + subset for subset in rest]
+
+# Input set
+input_set = [1, 2, 3]
+
+# Generate the power set
+result = power_set(input_set)
+
+# Print the result
+print("Power set:", result)
+
+# Verify with SymPy
+from sympy import FiniteSet, powerset
+
+input_set_sympy = FiniteSet(1, 2, 3)
+sympy_power_set = [list(subset) for subset in powerset(input_set_sympy)]
+
+print("SymPy power set:", sympy_power_set)
+
+if result == sympy_power_set:
+    print("The results match!")
+else:
+    print("The results do not match.")
 
 """
 import numpy as np
