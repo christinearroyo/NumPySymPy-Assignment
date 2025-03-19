0061dfe0ec805d6c31d2ed2f4c3f6245536c9bb3
Christine Arroyo <chrstinearroyo@gmail.com>
2025-03-19 13:00:09 +0800

    Update Arroyo_PS.py

--- Arroyo_PS.py
+++ Arroyo_PS.py
@@ -1,4 +1,32 @@
+#! C:\Users\chrst\OneDrive\Documents\GitHub\NumPySymPy-Assignment\myenv\Scripts\python.exe
+
 import numpy as np
+from sympy import FiniteSet
+
+set_a = {1,3,5,}
+set_b = {2, 1, 4, 3, 6, 5}
+
+def is_subset(set_a, set_b): 
+    array_a = np.array(list(set_a))
+    array_b = np.array(list(set_b))
+
+    result = np.isin(array_a, array_b).all()
+    return result
+
+print("Is set_a a subset of set_b? ", is_subset(set_a, set_b))
+
+def powerset(s):
+    s = FiniteSet(*s)
+    result = []
+
+    for subset in s.powerset():
+        result.append(subset)
+
+    return sorted(result)
+
+s = {1, 2, 3}
+print(powerset(s))
+
 
 def cartesian_product(*arrays):
     grid = np.meshgrid(*arrays)
